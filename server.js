b7937b7e6b01e18bd0c50c5beb5014945bcff797
Faisal Abuzaid <faisalabuzaid@outlook.com>
2021-04-22 10:18:27 +0300

    addStyle

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // define our method-override reference
-app.use(methodOverride('_mthod'));
+app.use(methodOverride('_method'));
 // Set the view engine for server-side templating
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -38,12 +38,16 @@ const client = new pg.Client(process.env.DATABASE_URL);
 app.get('/', renderHome);
 app.post('/save', saveQuote);
 app.get('/favorite-quote', renderFav);
+app.get('/quote/:id', getOneQuote);
+app.put('/quote/:id', updateOneQuote);
+app.delete('/quote/:id', deleteOneQuote);
+
 
 // callback functions
 // -- WRITE YOUR CALLBACK FUNCTIONS FOR THE ROUTES HERE --
 
 function renderHome (req, res) {
-    const url='https://thesimpsonsquoteapi.glitch.me/quotes?count=10'
+    const url='https://thesimpsonsquoteapi.glitch.me/quotes?count=10';
     superagent.get(url).set('User-Agent', '1.0').then(data => {
     
         res.render('index', {'data': data.body});
@@ -54,7 +58,6 @@ function saveQuote (req, res) {
     const {character, quote, image, characterDirection}=req.body;
     const values = [character, quote, image, characterDirection];
     const SQL ='INSERT INTO quotes (character, quote, image, direction) values ($1, $2, $3, $4)';
-    const SQL2 = 'SELECT * FROM quotes'
     console.log('values', values);
 
     client.query(SQL, values).then(() => {
@@ -67,9 +70,33 @@ function saveQuote (req, res) {
 function renderFav (req, res) {
     const SQL = 'SELECT * FROM quotes';
     client.query(SQL).then(dataTwo => {
-        console.log('data.rows', dataTwo.rows);
         res.render('favorite-quote', {'data': dataTwo.rows});
     });
+}
+
+function getOneQuote(req, res) {
+    const id = req.params.id;
+    const SQL='select * from quotes where id=$1';
+    client.query(SQL, [id]).then(data => {
+        res.render('quote', {'element': data.rows[0]});
+    })
+}
+
+function updateOneQuote(req, res) {
+    const id = req.params.id;
+    const SQL='UPDATE quotes SET quote=$1 where id=$2';
+    client.query(SQL, [req.body.quote, id]).then(data => {
+        getOneQuote(req, res);
+    })
+}
+
+
+function deleteOneQuote(req, res) {
+    const id = req.params.id;
+    const SQL='delete from quotes where id=$1';
+    client.query(SQL, [id]).then(data => {
+       renderFav(req,res);
+    })
 }
 // helper functions
 
